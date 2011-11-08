e7c592669343786e19148f7c490348ce9bba87ca
Adele Hedrick <adele@hedrick.ca>
2011-11-08 15:56:32 -0500

    First commit

---
+++ src/csci3010u/kooBeanz/SimulationIceActivity.java
@@ -0,0 +1,13 @@
+package csci3010u.kooBeanz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimulationIceActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
